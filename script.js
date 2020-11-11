2c1683708afa6a9e82c146ac610af36bfe3d43b1
Akshay Krishna <akshaykrishnakanrh@gmail.com>
2020-11-11 13:58:36 +0530

    testimonials section added

--- script.js
+++ script.js
@@ -126,6 +126,36 @@ $(document).ready(function () {
         }
     }
 
+
+    var $owl = $('#carousel1');
+    $owl.children().each(function (index){
+        $(this).attr('data-position', index);
+    });
+
+    $('#carousel1').owlCarousel({
+        loop:false,
+        center: true,
+        margin:0,
+        responsiveClass: true,
+        nav:false,
+        responsive: {
+            0: {
+                items: 1
+            }, 
+            680: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+        }
+    })
+
+    $(document).on('click', '.owl-item>div', function(){
+        var $speed = 1000;
+        $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]) 
+    })
+
     var modal = document.getElementById("myModal");
     var modalOverlay = document.getElementsByClassName("Modal__modal-overlay-div___RqVH9")[0]
 // Get the button that opens the modal
